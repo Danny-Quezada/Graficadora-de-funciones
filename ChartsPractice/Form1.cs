318ca25fcc9cfed706c6f30869dc64ebc89717fc
Danny-Quezada <danny.quezada32u@std.uni.edu.ni>
2021-11-05 16:17:22 -0600

    Agregando el método Fx para poder poner la función

--- ChartsPractice/Form1.cs
+++ ChartsPractice/Form1.cs
@@ -16,9 +16,14 @@ namespace ChartsPractice
 		{
 			InitializeComponent();
 		}
-
+		private void Fx(ref double x,ref double y)
+		{
+			//Aqui se puede poder cualquier función.
+			y = Math.Pow(x,2);
+		}
 		private void button1_Click(object sender, EventArgs e)
 		{
+			
 			double x=0; //Valor de x
 			double y = 0; // Valor de y
 			
@@ -29,9 +34,9 @@ namespace ChartsPractice
 				
 			
 				x = i;
-				
-				y=Math.Pow(x,3); // Esto es y=x^2
-				chart1.Series["Series1"].Color = Color.Black;
+
+				Fx(ref x, ref y); // Aqui tenes que poner X y Y
+				chart1.Series["Series1"].Color = Color.Black; // Este es el color de la gráfica. se le puede cambiar a cualquier color
 				chart1.Series["Series1"].Points.AddXY(x,y);
 			
 
@@ -55,9 +60,10 @@ namespace ChartsPractice
 			//En este for se grafica el area sombreada
 			for(double i=-2.0; i<0; i+=0.1) // la variable i son los limites. donde empieza y donde termina
 			{
-				 xA= i; 
-				yA = xA*xA*xA; //Esta misma ecuación es la primera que esta en el for de graficar la ecuación lo que quiere decir que cada vez que querramos el area sombreada tendremos que cambiar aquí también
+				 xA= i;
+				//yA =Math.Pow(Math.Sin(xA), 2);  //Esta misma ecuación es la primera que esta en el for de graficar la ecuación lo que quiere decir que cada vez que querramos el area sombreada tendremos que cambiar aquí también
 
+				Fx(ref xA, ref yA); // Aqui tambien se tiene que poner
 				chart1.Series["Series4"].Points.AddXY(xA, yA);  //Aqui se grafica el area sombreada.
 
 			}
@@ -69,15 +75,17 @@ namespace ChartsPractice
 
 		}
 
+		#region
+
+		#endregion
 		private void Form1_Load(object sender, EventArgs e)
 		{
-
+			#region 
 			chart1.Series["Series4"].Color = Color.Gray; // Este es el color del área sombreada
 			chart1.ChartAreas["ChartArea1"].AxisX.Minimum = -10.0; // Estos son los valores del eje x en los negativos
 			chart1.ChartAreas["ChartArea1"].AxisX.Interval = 0.5; // Este es el intervalo con lo que van a ir saliendo, ejemplo: -10.5, -11, -11.5
 			chart1.ChartAreas["ChartArea1"].AxisX.Maximum = 10.0; // Este es el valor máximo del eje x positivo.
-			
-			
+			#endregion
 			chart1.ChartAreas["ChartArea1"].AxisY.Interval = 0.5; // Intervalo en el eje Y
 			chart1.ChartAreas["ChartArea1"].AxisY.Minimum = -10.0; // Valores del eje Y en los negativos. 
 			chart1.ChartAreas["ChartArea1"].AxisY.Maximum = 10.0; // Valores del eje Y en los positivos.
